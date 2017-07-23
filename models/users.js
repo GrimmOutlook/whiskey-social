a54391c5335c4ccc660c2a8c54a9035d53d3d4a4
GrimmOutlook <grimmoutlook87@gmail.com>
2017-07-22 22:28:26 -0400

    Added passport.js local Strategy for signup.  Eliminated DOB field in user Schema.

--- models/users.js
+++ models/users.js
@@ -1,26 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  name: {
-    {firstName: String, required: true, trim: true},
-    {lastName: String, required: true, trim: true}
-  },
-  password: {
-    type: String,
-    required: true
-  },
+  firstName: {String, required: true, trim: true},
+  lastName: {String, required: true, trim: true},
+  password: {type: String, required: true},
   userName: {type: String, required: true, trim: true},
-  dob: {
-    {month: type: Number, required: true},
-    {day: type: Number, required: true},
-    {year: type: Number, required: true}
-  },
   email: {type: String, required: true, trim: true}
 });
 
 
 userSchema.virtual('fullName').get(function() {
-  return `${this.name.firstName} ${this.name.lastName}`;
+  return `${this.firstName} ${this.lastName}`;
 });
 
 userSchema.methods.formattedUser = function() {
@@ -29,7 +19,6 @@ userSchema.methods.formattedUser = function() {
     id: this._id,
     name: this.fullName,
     userName: this.userName,
-    dob: this.dob,
     email: this.email
   };
 }
