3f0f3ef2b2251b8efed9ea541aec7ca73c9460ec
GrimmOutlook <grimmoutlook87@gmail.com>
2017-08-11 07:16:12 -0400

    Updates to scraper - loop through a list of URLs, compiles it into JSON, and writes to whiskeys.json file.

--- commons/whiskey-scraper.js
+++ commons/whiskey-scraper.js
@@ -5,85 +5,60 @@ const fs = require('fs');
 const app = express();
 const port = 8000;
 
-request('https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true#productlist-filter', function(error, response, body) {
+const URL = ['https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=2#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=3#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=4#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=5#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=6#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=7#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=8#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=9#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=10#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=11#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=12#productlist-filter', 'https://www.thewhiskyexchange.com/c/33/american-whiskey?filter=true&pg=13#productlist-filter'];
 
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
+URL.forEach(function(URL){
 
-  var $ = cheerio.load(body);
+  request(URL, function(error, response, body) {
 
-    // For each div with item class get whiskeyName & smallImageURL ---------------------
-    $('.item').each(function(index){
+    if(error) {
+      console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
 
-      $(this).find('a').each(function(index) {
+    var $ = cheerio.load(body);
 
-        var largeImageURL;
-        var description;
-        var country;
-        var region;
-        const whiskeyName = $(this).attr('title');
-        const smallImageURL = $(this).find('.image-container img').attr('data-original');
+      // For each div with item class get whiskeyName & smallImageURL ---------------------
+      $('.item').each(function(index){
 
-        //Get description, country, region, largeImageURL from next page -----------------
-        const redirectURL = 'https://www.thewhiskyexchange.com' + $(this).attr('href');
-          request(redirectURL, function(error, response, body) {
-            $ = cheerio.load(body);
+        $(this).find('a').each(function(index) {
 
-            $('div#productDefaultImage').each(function(index){
-              largeImageURL = $(this).find('img').attr('data-original');
-            })
+          var largeImageURL;
+          var description;
+          var country;
+          var region;
+          const whiskeyName = $(this).attr('title');
+          const smallImageURL = $(this).find('.image-container img').attr('data-original');
 
-            $('div#prodDesc').each(function(index){
-              description = $(this).text();
-              // console.log(description);
-            })
+          //Get description, country, region, largeImageURL from next page -----------------
+          const redirectURL = 'https://www.thewhiskyexchange.com' + $(this).attr('href');
+            request(redirectURL, function(error, response, body) {
+              $ = cheerio.load(body);
 
-            $('div#prodMeta').each(function(index){
-              country = $('dl.meta dd:nth-child(2)').text();
-              region = $('dl.meta dd:nth-child(4)').text();
-            })
+              $('div#productDefaultImage').each(function(index){
+                largeImageURL = $(this).find('img').attr('data-original');
+              })
 
-            console.log('Whiskey: ' + whiskeyName + '  - Country: ' + country +', Region: ' + region + ', Description: ' + description + ', smallImageURL: ' + smallImageURL + ', largeImageURL: ' + largeImageURL);
+              $('div#prodDesc').each(function(index){
+                description = $(this).text();
+                // console.log(description);
+              })
 
-          })  // request redirect
-      })   // a each
-    })  // .item each
+              $('div#prodMeta').each(function(index){
+                country = $('dl.meta dd:nth-child(2)').text();
+                region = $('dl.meta dd:nth-child(4)').text();
+              })
 
-//----- This works, but doesn't match up whiskey w/items from next page ----------------
-    // $('.image-container img').each(function(index) {
-    //   const imageURL = $(this).attr('data-original');
-    //   const whiskeyName = $(this).attr('alt');
-    //   const whiskeyObject = {name: whiskeyName, image_url: imageURL};
-    //   whiskeys = JSON.stringify(whiskeyObject);
-    //   // console.log(whiskeys);
-    // })
+              console.log(whiskeyName);
+              // console.log('Whiskey: ' + whiskeyName + '  - Country: ' + country +', Region: ' + region + ', Description: ' + description + ', smallImageURL: ' + smallImageURL + ', largeImageURL: ' + largeImageURL);
 
-    // $('.item a').each(function(index) {
-    //   const redirectURL = 'https://www.thewhiskyexchange.com' + $(this).attr('href');
-    //   // console.log(redirectURL);
+              fs.appendFileSync('whiskeys.json', '{"whiskeyName": "' + whiskeyName + '", "country": "' + country + '", "region": "' + region + '", "description": "' + description + '", "smallImageURL": "' + smallImageURL + '", "largeImageURL": "' + largeImageURL +'"}');
 
-    //     request(redirectURL, function(error, response, body) {
-    //       $ = cheerio.load(body);
-
-    //         $('div#prodDesc').each(function(index){
-    //           const description = $(this).text();
-    //           console.log(description);
-    //         })
-
-    //         $('div#prodMeta').each(function(index){
-    //           country = $('dl.meta dd:nth-child(2)').text();
-    //           region = $('dl.meta dd:nth-child(4)').text();
-    //           // console.log('Country: ' + country +', Region: ' + region);
-    //         })
-    //     })
-    // })
-
-    // console.log('Whiskeys: ' + whiskeys + 'Country: ' + country + 'Region: ' + region);
-
-
-});   // 1st request URL
+            })  // request redirect
+        })   // a each
+      })  // .item each
+    });   // 1st request URL
+});  // URL forEach
 
 app.listen(port);
 console.log(`Stuff is working on Port ${port}!`);
