687a436e55b334bd8e6653abacc9e44dcbaacfa9
GrimmOutlook <grimmoutlook87@gmail.com>
2017-09-03 21:49:01 -0400

    whiskey-post POST method - resolved all Promises values into one fxn.  Will write values in appropriate places in the DB next.

--- routes/whiskeyRouter.js
+++ routes/whiskeyRouter.js
@@ -85,34 +85,68 @@ router.get('/:userId/post/:whiskeyId', (req, res) => {
           //POST info entered into screen into DB
 router.post('/:userId/post/:whiskeyId', (req, res) => {
   //GET comment and rating from form:
-    var userInput = JSON.stringify(req.body);
+    // var userInput = JSON.stringify(req.body);
+    var userInput = req.body;
     console.log(userInput);
+  //   console.log(req.params.whiskeyId);
 
-  //GET user id from hard-coded userId
-  User
-    .findById(req.params.userId)
-    .exec()
-    .then(user => {
-      console.log('Here is user.username info.: ' + user.username);
-      return user;
-    })
-    .then(
-  //GET whiskey info from params.id  All info, including URLs?
-      Whiskey
-        .findById(req.params.whiskeyId)
-        .exec()
-        .then((single_whiskey, user) => {
-          console.log('findById passed to fxn: ' + single_whiskey);
-          console.log('Does user.posts make it down here? ' + user.posts);  //not now
-          console.log('userInput: ' + userInput);
-          res.render('post-confirm', single_whiskey)
-          })
-    )
-    .catch(
-      err => {
-        console.error(err);
-        res.status(500).json({message: 'Something\'s wrong with POSTing a post.'});
-    });
+  // //GET user id from hard-coded userId
+  // User
+  //   .findById(req.params.userId)
+  //   .exec()
+  //   .then(user => {
+  //     console.log('User is: ' + user.username);
+      // console.log('Whiskey req.params.whiskeyID: ' + Whiskey.findById(req.params.whiskeyId));
+  var a = Whiskey.findById(req.params.whiskeyId)
+          .then((doesntMatter) => {
+          return [doesntMatter.whiskeyName, doesntMatter.largeImageURL, doesntMatter.smallImageURL];
+        });
+    //     .then(single_whiskey => {
+    //       // console.log('Whiskey.findById: ' + Object.keys(single_whiskey));
+    //       console.log('single_whiskey: ' + single_whiskey);
+    //       // console.log('Does user.posts make it down here? ' + user.posts);  //not now
+    //       console.log('userInput: ' + userInput);
+    //       console.log('userJustWork: ' + User.findById('598786f0873d1cf2aa55467c'));
+    //       // console.log('Here is user.username info. again: ' + user.username);
+    //       // res.render('post-confirm', single_whiskey);
+    //       return single_whiskey;
+    // })
+  var b = User.findById(req.params.userId)
+          .then(doesntMatterEither => {
+          return doesntMatterEither;
+        });
+
+          //values is an array of the 3 vars return values
+          //manipulate these values to add whiskey info & userInput info into user.posts
+  Promise.all([a,b, userInput]).then(values => {
+    console.log('User\'s posts array: ' + values[1].posts);
+    console.log('whiskeyName & both image URLs: ' + values[0]);
+    console.log('userInput comment & rating: ' + values[2].comment + values[2].rating);
+    res.render('post-confirm', values);
+  });
+
+    // .then((single_whiskey => {
+    //   console.log('userJustWork: ' + User.findById(req.params.userId));
+    //   console.log('does single_whiskey show up here?: ' + single_whiskey);
+    //   })
+    // )
+
+  // //GET whiskey info from params.id  All info, including URLs?
+  //     Whiskey
+  //       .findById(req.params.whiskeyId)
+  //       .exec()
+  //       .then((single_whiskey, user) => {
+  //         console.log('findById passed to fxn: ' + single_whiskey);
+  //         console.log('Does user.posts make it down here? ' + user.posts);  //not now
+  //         console.log('userInput: ' + userInput);
+  //         res.render('post-confirm', single_whiskey)
+  //         })
+  //   )
+    // .catch(
+    //   err => {
+    //     console.error(err);
+    //     res.status(500).json({message: 'Something\'s wrong with POSTing a post.'});
+    // });
     //Determine updateable fields and how to update the User's posts array of objects
 
 
